c903b4c9edcae8192357c4c43a2159f10fad3cf7
Matthias Hummel <ma-hummel@gmx.de>
2023-12-05 09:48:42 +0100

    first actor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,30 @@
+import akka.stream.ActorMaterializer
+import akka.actor.{ActorSystem, Props}
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import akka.pattern.ask
+import akka.util.Timeout
+
 object Main extends App {
   println("Hello, World!")
+
+  implicit val system: ActorSystem = ActorSystem("APIActorSystem")
+
+  val apiClientActor = system.actorOf(Props[APIClientActor], "apiClientActor")
+
+  // Sending a message to the actor to initiate the API call
+  implicit val timeout: Timeout = Timeout(10.seconds)
+  val futureResult = apiClientActor ? FetchData
+
+  // Wait until the FetchData message is processed by the actor
+  try {
+    Await.result(futureResult, 10.seconds)
+  } catch {
+    case ex: Exception =>
+      println(s"An error occurred: ${ex.getMessage}")
+  } finally {
+    // Terminate the actor system after FetchData is executed or timed out
+    system.terminate()
+  }
 }
+
